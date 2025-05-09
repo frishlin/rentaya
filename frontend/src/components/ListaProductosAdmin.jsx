8cd7a7c075385e1d92feb00725bfb5346f850733
frishlin <frishlin@gmail.com>
2025-05-09 12:18:08 -0600

    Agregando botón Registrar vehículo en la ruta /admin/lista

--- frontend/src/components/ListaProductosAdmin.jsx
+++ frontend/src/components/ListaProductosAdmin.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import './ListaProductosAdmin.css'
+import { Link } from 'react-router-dom';
 
 const ListaProductosAdmin = () => {
     const [productos, setProductos] = useState([]);
+    const [mensaje, setMensaje] = useState('');
 
     useEffect(() => {
         const fetchProductos = async () => {
@@ -27,19 +29,25 @@ const ListaProductosAdmin = () => {
             });
             if(response.ok) {
                 setProductos(productos.filter(p => p.id !== id));
+                setMensaje('El producto ha sido eliminado exitosamente')
             }
             else {
-                alert('Ocurrió un error al eliminar el producto');
+                setMensaje('Ocurrió un error al eliminar el producto');
             }
         }
         catch (error) {
             console.error('Ocurrió un error al eliminar el producto', error);
+            setMensaje('Ocurrió un error de conexión al eliminar el producto');
         }            
     };
 
     return(
         <section className='admin-container'>
             <h2>Listado de productos</h2>
+            {mensaje && <p className='mensaje'>{mensaje}</p> }
+            <Link to='/admin/registro'>
+                <button className='boton-volver'>Registrar nuevo vehículo</button>
+            </Link>
             <table className='admin-table'>
                 <thead>
                     <tr>
