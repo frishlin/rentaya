35bc11377c4205e3ed7c8c08b1615351e02dc3c3
frishlin <frishlin@gmail.com>
2025-06-20 12:25:32 -0600

    Implementando buscador por categorías

--- frontend/src/components/ProductoList.jsx
+++ frontend/src/components/ProductoList.jsx
@@ -4,33 +4,42 @@ import { Link } from 'react-router-dom';
 
 const ProductoList = ({ filtrosBusqueda }) => {
   const [productos, setProductos] = useState([]);
+  const [categorias, setCategorias] = useState([]);
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
 
   useEffect(() => {
-    const fetchProductos = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8080/productos');
-        const data = await response.json();
+        const [resProductos, resCategorias] = await Promise.all([
+          fetch('http://localhost:8080/productos'),
+          fetch('http://localhost:8080/categorias')
+
+        ]);
+        const dataProductos = await resProductos.json();
+        const dataCategorias = await resCategorias.json();
+        setCategorias(dataCategorias);
 
         if(!filtrosBusqueda.ciudad) {
-          const productosUnicos = Array.from(new Map(data.map(p => [p.id, p])).values());
+          const productosUnicos = Array.from(new Map(dataProductos.map(p => [p.id, p])).values());
           const aleatorios = productosUnicos.sort(() => 0.5 - Math.random()).slice(0, 10);
           setProductos(aleatorios);
           
         } else {
-          setProductos(data);
+          setProductos(dataProductos);
         }
       } catch (error) {
-        console.error('Error al obtener los productos:', error);
+        console.error('Error al obtener los productos o categorías:', error);
       }
     };
 
-    fetchProductos();
+    fetchData();
   }, [filtrosBusqueda.ciudad]);
 
   const ciudadBuscada = filtrosBusqueda?.ciudad.toLowerCase() || '';
   
   const productosFiltrados = productos.filter(producto =>
-    producto.nombre.toLowerCase().includes(ciudadBuscada)
+    producto.nombre.toLowerCase().includes(ciudadBuscada) &&
+    (categoriaSeleccionada === '' || producto.categoria?.id === parseInt(categoriaSeleccionada))
   );
 
   if (productos.length === 0) {
@@ -58,6 +67,19 @@ const ProductoList = ({ filtrosBusqueda }) => {
       <h2 className="titulo-productos" style={{ color: '#ffc107' }}>
         Vehículos disponibles actualmente
       </h2>
+
+      <div className='filtro-categorias'>
+        <label htmlFor="categoria">Selecciona una categoría: </label>
+        <select id="categoria" value={categoriaSeleccionada} onChange={(e) => setCategoriaSeleccionada(e.target.value)}>
+          <option value="">Todas</option>
+          {categorias.map(categoria => (
+            <option key={categoria.id} value={categoria.id}>{categoria.titulo}
+            </option>
+          ))}
+        </select>
+      </div>
+
+
       <div className="productos-grid">
         {productosFiltrados.map(producto => (
           <Link to={`/producto/${producto.id}`} key={producto.id} className='producto-card'>
